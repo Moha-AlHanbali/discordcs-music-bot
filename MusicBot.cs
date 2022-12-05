216aac2f462f3df3ecdc500c06c555f6e4654e52
Moha-AlHanbali <moha.alhanbali@gmail.com>
2022-12-05 23:06:23 +0300

    fix: cleanup

--- MusicBot.cs
+++ MusicBot.cs
@@ -1,6 +1,5 @@
 ﻿namespace MusicBot
 {
-
     using DSharpPlus;
     using DSharpPlus.CommandsNext;
     using Microsoft.Extensions.Logging;
@@ -9,16 +8,17 @@
     {
         static void Main(string[] args)
         {
-            MainAsync().GetAwaiter().GetResult();
-        }
-
-        static async Task MainAsync()
-        {
             var root = Directory.GetCurrentDirectory();
             var dotenv = Path.Combine(root, ".env");
             DotEnv.Load(dotenv);
 
-            var discord = new DiscordClient(new DiscordConfiguration()
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        internal static async Task MainAsync()
+        {
+
+            var bot = new DiscordClient(new DiscordConfiguration()
             {
                 Token = Environment.GetEnvironmentVariable("TOKEN"),
                 TokenType = TokenType.Bot,
@@ -27,25 +27,19 @@
                 MinimumLogLevel = LogLevel.Debug
 
             });
+
             Console.WriteLine("ONLINE");
 
-            discord.MessageCreated += async (s, e) =>
-                      {
-                        Console.WriteLine("READING");
-                          if (e.Message.Content.ToLower().StartsWith("ping")){
-                            Console.WriteLine("PINGING");
-                            await e.Message.RespondAsync("pong!");
-                          }
-                      };
-
-            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            var commands = bot.UseCommandsNext(new CommandsNextConfiguration()
             {
                 StringPrefixes = new[] { "!" }
             });
 
             commands.RegisterCommands<BotCommands>();
 
-            await discord.ConnectAsync();
+            Console.WriteLine("REGISTERED COMMANDS");
+
+            await bot.ConnectAsync();
             await Task.Delay(-1);
         }
     }
