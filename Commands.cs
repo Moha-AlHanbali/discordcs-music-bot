4b4af3a2a5c7fe2bbbc534cb8a0de23f9ea8b7ea
Moha-AlHanbali <moha.alhanbali@gmail.com>
2022-12-07 23:51:46 +0300

    feat: can play music files locally

--- Commands.cs
+++ Commands.cs
@@ -1,11 +1,13 @@
 
 namespace MusicBot
 {
+    using System.Text.RegularExpressions;
+    using System.IO;
+    using System.Diagnostics;
     using DSharpPlus.Entities;
     using DSharpPlus.VoiceNext;
     using DSharpPlus.CommandsNext;
     using DSharpPlus.CommandsNext.Attributes;
-
     public class BotCommands : BaseCommandModule
     {
         [Command("join")]
@@ -16,6 +18,7 @@ namespace MusicBot
             {
                 channel ??= context.Member?.VoiceState?.Channel;
                 await context.RespondAsync($"Joining {channel?.Name} . . .");
+
                 await channel.ConnectAsync();
             }
             catch
@@ -38,7 +41,7 @@ namespace MusicBot
                 }
                 else
                 {
-                    await context.RespondAsync("Not joined to a VC..");
+                    await context.RespondAsync("Not joined to a channel..");
                 }
             }
             catch
@@ -49,10 +52,40 @@ namespace MusicBot
         }
 
         [Command("play")]
-        public async Task PlayCommand(CommandContext context)
+        public async Task PlayCommand(CommandContext context, params string[] path)
         {
-            await context.RespondAsync("Playing . . .");
+            string joinedPath = string.Join(" ", path);
+            Console.WriteLine(joinedPath);
+            if (joinedPath.Trim() != "" && joinedPath.Trim() != null)
+            {
+                // string trimmedPath = Regex.Replace(path, " ", "20%");
+                Console.WriteLine($"Started playing {joinedPath}");
+                var voiceNext = context.Client.GetVoiceNext();
+                var connection = voiceNext.GetConnection(context.Guild);
+                var transmit = connection.GetTransmitSink();
+                var pcm = ConvertAudioToPcm(joinedPath);
+                await pcm.CopyToAsync(transmit);
+                await pcm.DisposeAsync();
+                await context.RespondAsync($"Playing {joinedPath}");
+            }
+            else
+            {
+                await context.RespondAsync("Please specify a track to play. . .");
+            }
+
         }
+        private Stream ConvertAudioToPcm(string filePath)
+        {
+            var ffmpeg = Process.Start(new ProcessStartInfo
+            {
+                FileName = "ffmpeg",
+                Arguments = $@"-i ""{filePath}"" -ac 2 -f s16le -ar 48000 pipe:1",
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            });
+            return ffmpeg.StandardOutput.BaseStream;
+        }
+
     }
 
 }
